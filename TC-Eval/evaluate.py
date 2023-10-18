7983f67b47d7a7da404b271fffaa6f9b0c9defb7
Yi-Chang Chen <ycc.tw.email@gmail.com>
2023-10-18 16:51:47 +0800

    [feature] Change interface of Task

--- TC-Eval/evaluate.py
+++ TC-Eval/evaluate.py
@@ -4,33 +4,44 @@ import numpy as np
 
 
 class Task:
-    def evaluate(self, list_of_data: List[Dict], list_of_response: List[str]) -> Dict:
-        pass
+    def evaluate(self, list_of_response: List[Dict]) -> Dict:
+        raise NotImplementedError
         # return metrics
 
 
 class MultipleChoiceTask(Task):
     def _extract_choice(self, response):
-        pass
+        raise NotImplementedError
 
-    def evaluate(self, list_of_data: List[Dict], list_of_response: List[str]) -> Dict:
-        # keys of data: gold
+    def evaluate(self, list_of_response: List[Dict]) -> Dict:
         correct_list = []
-        for data, response in zip(list_of_data, list_of_response):
-            choice = self._extract_choice(response)
-            correct_list.append(1 if choice == data['gold'] else 0)
+        gold_dict= self._gold_dict
+
+        response_dict = {}
+        for data in list_of_response:
+            response_dict[data['id']] = data['response']
+
+        for idx in self._gold_dict.keys():            
+            choice = self._extract_choice(response_dict[idx])
+            correct_list.append(1 if choice == gold_dict[idx] else 0)
         return {
             'accuracy': np.mean(correct_list)
         }
 
 
 class QuestionAnsweringTask(Task):
-    def evaluate(self, list_of_data: List[Dict], list_of_response: List[str]) -> Dict:
+    def evaluate(self, list_of_response: List[Dict]) -> Dict:
         pass
 
 
 class SummaryTask(Task):
-    def evaluate(self, list_of_data: List[Dict], list_of_response: List[str]) -> Dict:
+    def evaluate(self, list_of_response: List[Dict]) -> Dict:
         pass
 
 
+class TTQATask(MultipleChoiceTask):
+    pass
+
+EVALUATION_ITEMS = [
+    ('TTQA', TTQATask('./data/TTQA/')),
+]
